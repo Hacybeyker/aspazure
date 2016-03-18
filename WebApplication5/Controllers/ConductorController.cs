7b2bace50090e4a24730a3286c2ebd2f79de5e29
Carlos Alfredo Osorio Perez <Hacybeyker@hotmail.com>
2016-03-17 19:04:18 -0500

    asp azure mostrar datos

---
+++ WebApplication5/Controllers/ConductorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class ConductorController : Controller
+    {
+        // GET: Conductor
+
+        webventapasajeEntities entidad = new webventapasajeEntities();
+        public ActionResult Index()
+        {
+            var listaConductor = entidad.conductor;
+            return View(listaConductor.ToList());
+        }
+    }
+}
---
+++ WebApplication5/Controllers/PasajeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class PasajeController : Controller
+    {
+        // GET: Pasaje
+        webventapasajeEntities entidad = new webventapasajeEntities();
+        public ActionResult Index()
+        {
+            var listapasajes = entidad.pasaje;
+            return View(listapasajes.ToList());
+        }
+    }
+}
